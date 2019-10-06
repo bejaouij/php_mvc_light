b3de7bffbdf98d3b78bb6ada521a0c3b31aabc96
bejaouij <bejaoui.je@gmail.com>
2019-10-06 22:53:58 +0200

    feat($model): Add model methods
    Add methods to the model class.

--- app/models/Model.php
+++ app/models/Model.php
@@ -2,21 +2,100 @@
     namespace App\Models;
 
     use App\Interfaces\Database\CRUD;
-
-    require_once('../interfaces/database/CRUD.php');
+    use App\Helpers\Core\Query_Builder\QueryBuilder;
+    use App\Helpers\Core\Config_Manager\ConfigManager;
 
     abstract class Model implements CRUD
     {
-        # TODO
-//        public static function createMany(Array $models) : int;
-//        public static function readBy(string $field, string $value) : Array;
-//        public static function readAll() : Array;
-//        public static function updateMany(Array $models): int;
-//        public static function deleteMany(Array $models) : int;
-//        public static function deleteById(string $id) : int;
-//
-//        public function create() : Model;
-//        public function readById($id) : Model;
-//        public function update() : Model;
-//        public function delete() : Model;
+        static protected $tableName;
+        static protected $primaryKey = 'id';
+
+        # TODO IMPLEMENT A HIDDEN FIELDS FEATURE
+
+        private $_data = array();
+
+        private static function getMostLikelyTableName() {
+            return get_called_class()::$tableName ? get_called_class()::$tableName : (strtolower((new \ReflectionClass(get_called_class()))->getShortName()));
+        }
+
+        private static function getDatabaseSchema() {
+            return ConfigManager::getDatabaseConfig('schema');
+        }
+
+        public static function createMany(Array $models) : int {
+            throw new \Exception('Not implemented.');
+        }
+
+        public static function readById(string $id) : Model {
+            $queryBuilder = new QueryBuilder();
+
+            $calledClass = get_called_class();
+            $model = new $calledClass();
+
+            $data = $queryBuilder->query('SELECT * FROM ' . self::getDatabaseSchema() . '.' . self::getMostLikelyTableName() . ' WHERE ' . get_called_class()::$primaryKey . ' = ?', [$id]);
+
+            if(count($data) > 0) {
+                $data = array_shift($data);
+            }
+
+            $model->hydrate($data);
+
+            return $model;
+        }
+
+        public static function readBy(string $field, string $value) : array {
+            $queryBuilder = new QueryBuilder();
+
+            return $queryBuilder->query('SELECT * FROM ' . self::getDatabaseSchema() . '.' . self::getMostLikelyTableName() . ' WHERE ' . $field . ' = ?', [$value]);
+        }
+
+        public static function readAll() : array {
+            $queryBuilder = new QueryBuilder();
+
+            return $queryBuilder->query('SELECT * FROM ' . self::getDatabaseSchema() . '.' . self::getMostLikelyTableName());
+        }
+
+        public static function updateMany(array $models): int {
+            throw new \Exception('Not implemented.');
+        }
+
+        public static function deleteMany(array $models) : int {
+            throw new \Exception('Not implemented.');
+        }
+
+        public static function deleteById(string $id) : bool {
+            throw new \Exception('Not implemented.');
+        }
+
+        public static function deleteBy(string $field, string $value) : int {
+            throw new \Exception('Not implemented.');
+        }
+
+        public function create() : Model {
+            throw new \Exception('Not implemented.');
+        }
+
+        public function update() : Model {
+            throw new \Exception('Not implemented.');
+        }
+
+        public function delete() : Model {
+            throw new \Exception('Not implemented.');
+        }
+
+        private function hydrate(array $data) : Model {
+            foreach($data as $column => $value) {
+                $this->setData($column, $value);
+            }
+
+            return $this;
+        }
+
+        public function getData(string $field = NULL) {
+            return !is_null($field) ? $this->_data[$field] : $this->_data;
+        }
+
+        public function setData(string $field, string $value) : void {
+            $this->_data[$field] = $value;
+        }
     }
