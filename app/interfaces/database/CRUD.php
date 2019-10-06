b5a2cfe619356d58a8676bda58dd68018f4ac69f
bejaouij <bejaoui.je@gmail.com>
2019-10-06 15:35:38 +0200

    fix($dataaccess): Correct return type
    - Use the Model class to return it properly.
    - Return the 'array' type instead of the 'Array' one.

--- app/interfaces/database/CRUD.php
+++ app/interfaces/database/CRUD.php
@@ -1,13 +1,15 @@
 <?php
 	namespace App\Interfaces\Database;
 
+	use App\Models\Model;
+
 	interface CRUD
 	{
 		public static function createMany(Array $models) : int;
-		public static function readBy(string $field, string $value) : Array;
-		public static function readAll() : Array;
-		public static function updateMany(Array $models): int;
-		public static function deleteMany(Array $models) : int;
+		public static function readBy(string $field, string $value) : array;
+		public static function readAll() : array;
+		public static function updateMany(array $models): int;
+		public static function deleteMany(array $models) : int;
 		public static function deleteById(string $id) : int;
 
 		public function create() : Model;
